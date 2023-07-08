bbe14c20a804875769e5e29e25b606d7d4a7aaf2
diamondhanddevOps <108594160+diamondhanddevOps@users.noreply.github.com>
2023-07-08 12:16:11 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,15 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-file-ur"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-  }
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::terraform-state-file-ur"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+      "Resource": "arn:aws:s3:::terraform-state-file-ur/path/to/my/key"
+    }
+  ]
 }
